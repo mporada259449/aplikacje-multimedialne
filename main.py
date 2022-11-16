f50d5ba57d23235699cb0760bf2b9c3583be89f6
mporada259449 <259449@student.pwr.edu.pl>
2022-11-17 00:15:18 +0100

    logging

--- main.py
+++ main.py
@@ -5,18 +5,50 @@ class Hub:
     def __init__(self):
         self.userFile = "users.db"
         self.connUsers = sqlite3.connect(self.userFile)
+        self.c = self.connUsers.cursor()
         self.loggedUsers = []
 
     def login(self, password, login):
-        pass
-
+        self.c.execute("""
+            SELECT password FROM users
+            WHERE name==?
+        """, (login,))
+        ans = self.c.fetchall()
+        self.connUsers.commit()
+        if ans == []:
+            return False
+        elif password==ans[0][0]:
+            self.loggedUsers.append(login)
+            return True
+        else:
+            return False 
+        
     def createAccount(self, login, password):
-        pass
+        self.c.execute("""
+            SELECT name FROM users
+            WHERE name==?
+        """,(login,))
+        ans = self.c.fetchall()
+        self.connUsers.commit()
+        if ans == []:
+            self.c.execute("""
+                INSERT INTO users(name, password) VALUES(?,?)
+            """,(login, password))
+            self.connUsers.commit()
+            return True
+        else:
+            return False 
 
     def startDevice(self):
         pass
 
     def startApp(self):
+        pass
+
+    def stopDevice(self):
+        pass
+    
+    def stopApp(self):
         pass
 
     def addDevice(self):
@@ -26,15 +58,15 @@ class Hub:
         pass
 
     def restartHub(self):
-        pass
+        self.loggedUsers = []
+
+    def powerOff(self):
+        self.connUsers.close()
+
 if __name__=="__main__":
     hub = Hub()
-    c = hub.connUsers.cursor()
-
-    c.execute("SELECT * FROM users")
-    ans  = c.fetchall()
-    hub.connUsers.commit()
-    hub.connUsers.close()
-    print(ans)
+    print(hub.createAccount(login="login02", password="haslo1234"))
+    print(hub.login(password="haslo1234", login="login02"))
+    hub.powerOff()
     #while True:
     #    pass
