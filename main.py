f916837e3704872f631cdc7166b32aaa83cd7ab3
mporada259449 <259449@student.pwr.edu.pl>
2022-11-16 13:12:15 +0100

    sketch of app

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hello world")
+import sqlite3
+
+
+class Hub:
+    def __init__(self):
+        userFile = "users.db"
+        confFile = "configurations.db"
+        loggedUsers = []
+
+    def login(self, password, login):
+        pass
+
+    def createAccount(self, login, password):
+        pass
+
+    def connectDatabase(self):
+        pass
+
+    def startDevice(self):
+        pass
+
+    def startApp(self):
+        pass
+
+    def addDevice(self):
+        pass
+
+    def addApp(self):
+        pass
+
+    def restartHub(self):
+        pass
+
+if __name__=="__main__":
+    hub = Hub()
+    hub.connectDatabase()
+    #while True:
+    #    pass
