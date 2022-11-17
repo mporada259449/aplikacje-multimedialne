a28858d956e597d83a9d434611f20b6b1c5ef2c1
mporada259449 <259449@student.pwr.edu.pl>
2022-11-17 22:12:18 +0100

    adding to configuration

--- createData.py
+++ createData.py
@@ -7,13 +7,22 @@ c = conn.cursor()
 #c.execute("""CREATE TABLE users (
 #    id INTEGER PRIMARY KEY AUTOINCREMENT,
 #    name TEXT,
-#    password TEXT
+ #  password TEXT
 #)""")
 #conn.commit()
 #myusers = [('jan', 'pass'), ('xyzuser','strongpass'), ('user1', 'haslo'), ('app', '1234')]
 #c.executemany("INSERT INTO users(name, password) VALUES(?,?)", myusers)
 #conn.commit()
-c.execute("SELECT * FROM users")
-print(c.fetchall())
+#c.execute("SELECT * FROM users")
+#print(c.fetchall())
+c.execute("""DROP TABLE configuration""")
+c.execute("""
+   CREATE TABLE configuration (
+        name TEXT,
+        isdevice INTEGER,
+        user_id INTEGER,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+""")
 conn.commit()
 conn.close()
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Hub:
         self.connUsers = sqlite3.connect(self.userFile)
         self.c = self.connUsers.cursor()
         self.loggedUsers = []
+        self.usedDevices = []
 
     def login(self, password, login):
         self.c.execute("""
@@ -39,8 +40,12 @@ class Hub:
         else:
             return False 
 
-    def startDevice(self):
-        pass
+    def startDevice(self, device, login):
+        self.c.execute("""SELECT id FROM users WHERE name==?""",(login,))
+        loginid = self.c.fetchall()
+        self.c.execute("""SELECT * FROM configuration
+                            WHERE name==? AND user_id==?
+        """,(device,loginid[0][0]))
 
     def startApp(self):
         pass
@@ -51,11 +56,62 @@ class Hub:
     def stopApp(self):
         pass
 
-    def addDevice(self):
-        pass
+    def addConf(self, username, conf, isDevice):
+        #jeżeli jest zalogowany
+        if username in self.loggedUsers:
+            #znajdź id
+            self.c.execute("""
+                SELECT id FROM users
+                WHERE name==?
+            """,(username,))
+            userid = self.c.fetchone()
+            #znajdz czy istnieje konfiguracja
+            self.c.execute("""
+                SELECT * FROM configuration
+                WHERE name==? AND user_id==?
+            """,(conf, userid[0]))
+            #jeżeli nie istnieje to dodaj
+            if self.c.fetchall() == []:
+                self.c.execute("INSERT INTO configuration VALUES(?,?,?)", (conf, isDevice, userid[0]))
+                self.connUsers.commit()
+                return True
+            else:
+                #jeśli konfiguracja jest już zapisana
+                print("Configuration has been already added")
+                return False
+        else:
+            #jeśli nie jesteś zalogowany
+            print("You are not logged.")
+            return False
 
-    def addApp(self):
-        pass
+
+    #def addApp(self, username, service):
+    #    #jeżeli jest zalogowany
+    #    if username in self.loggedUsers:
+    #        #znajdź id
+    #        self.c.execute("""
+    #            SELECT id FROM users
+    #            WHERE name==?
+    #        """,(username,))
+    #        userid = self.c.fetchone()
+    #        #znajdz czy istnieje konfiguracja
+    #        self.c.execute("""
+    #            SELECT * FROM configuration
+    #            WHERE name==? AND user_id==?
+    #        """,(service, userid[0]))
+    #        #jeżeli nie istnieje to dodaj
+    #        if self.c.fetchall() == []:
+    #            self.c.execute("INSERT INTO configuration VALUES(?,?,?)", (service, 0, userid[0]))
+    #            self.connUsers.commit()
+    #            return True
+    #        else:
+    #            #jeśli konfiguracja jest już zapisana
+    #            print("Service has been already added to configuration")
+    #            return False
+    #    else:
+    #        #jeśli nie jesteś zalogowany
+    #        print("You are not logged.")
+    #        return False
 
     def restartHub(self):
         self.loggedUsers = []
@@ -65,8 +121,10 @@ class Hub:
 
 if __name__=="__main__":
     hub = Hub()
-    print(hub.createAccount(login="login02", password="haslo1234"))
-    print(hub.login(password="haslo1234", login="login02"))
+    hub.loggedUsers.append("jan")
+    print(hub.addConf(username="jan", conf="Netflix", isDevice=False))
+    hub.c.execute("SELECT * FROM configuration")
+    print(hub.c.fetchall())
     hub.powerOff()
     #while True:
     #    pass
